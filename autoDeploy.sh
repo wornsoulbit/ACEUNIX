3b770aa013996e52abaf5ecb2ae329af9e8b8d20
evericain01 <evericain01@hotmail.com>
2020-12-07 13:40:17 -0500

    Updated autoDeploy file

--- autoDeploy.sh
+++ autoDeploy.sh
@@ -2,12 +2,11 @@
 
 #In order for this script to work you need to be in your local git repository (ACEUNIX directory)
 
-$(git pull)
+upToDateMessage=$(git pull)
 
 
-if [[ `git status --porcelain` ]];
+if ! [[ $upToDateMessage == "Already up to date." ]];
 then
-	$(touch file.txt)
 	#(1)build website
 	#(2)copy static files to the correct location 
 fi	
