06a8975f24b9af5232ea8ea12221b3886139037a
Debian <debian@vps-06d9c1c1.vps.ovh.ca>
2020-12-10 20:21:07 +0000

    editing the autoDeploy.sh file to hopefully copy properly this time

--- autoDeploy.sh
+++ autoDeploy.sh
@@ -2,14 +2,16 @@
 
 #In order for this script to work you need to be in your local git repository (ACEUNIX directory)
 
+cd /home/debian/ACEUNIX/
+
 upToDateMessage=$(git pull)
 
 
 if ! [[ $upToDateMessage == "Already up to date." ]];
 then
 	#(1)building site
-	cd ~/ACEUNIX/jekyll-theme-chirpy
+	cd /home/debian/ACEUNIX/jekyll-theme-chirpy
 	bundle exec jekyll build
 	#(2)copy static files to the correct location
-	sudo rsync -r _site/* /var/www/html
+	sudo rsync -r home/debian/ACEUNIX/jekyll-theme-chirpy/_site/* /var/www/html
 fi	
